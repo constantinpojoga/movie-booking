889bfbc8c375372c62d5322b466e5d6d46991187
Constantin Pojoga <pojogac@gmail.com>
2016-11-03 23:29:08 -0500

    test email ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,8 +55,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "codechallenge_#{Rails.env}"
-  config.action_mailer.perform_caching = false
-    config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
